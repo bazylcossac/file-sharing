322ea19fb4c0546ce7281f7181ea83ce21c47e62
bazylcossac <bazyl.cossac@gmail.com>
2025-07-23 21:30:55 +0200

    test zmiana

--- frontend/src/components/HeaderUser.tsx
+++ frontend/src/components/HeaderUser.tsx
@@ -10,9 +10,7 @@ function HeaderUser() {
     return session.data?.user;
   }, [session]);
 
-  /// testowa zmiana
-
-  /// testowa zmiana z maina
+  ///
   return (
     <div className="flex flex-row items-center">
       <div className="flex items-center gap-2 hover:bg-neutral-700 transition rounded-md px-2 py-1 cursor-pointer">
@@ -39,8 +37,8 @@ function HeaderUser() {
           }
         />
       </div>
-      <p className="font-bold text-white/50 mx-2 hidden md:inline-block">/</p>
-
+      <p className="font-bold text-white/50 mx-2 hidden md:inline-block">siema zmianna main</p>
+          
       <div className="flex items-center gap-2 hover:bg-neutral-700 transition rounded-md px-2 py-2 cursor-pointer text-xs">
         <p>Test Folder</p>
       </div>
