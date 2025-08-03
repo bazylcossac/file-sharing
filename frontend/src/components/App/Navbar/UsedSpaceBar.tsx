051e02f89415b0610694915754a798c0c82453c3
bazylcossac <bazyl.cossac@gmail.com>
2025-08-03 19:32:22 +0200

    add error and loadng state

--- frontend/src/components/App/Navbar/UsedSpaceBar.tsx
+++ frontend/src/components/App/Navbar/UsedSpaceBar.tsx
@@ -1,19 +1,35 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useUserSpace } from "@/hooks/components/Navbar/getUsedSpace";
 import { getBytesToGb } from "@/utils/bytesToGb";
 
 import { useMemo } from "react";
 
 export default function UsedSpaceBar() {
-  const { data, isLoading, isError } = useUserSpace();
+  const { data, isLoading, isError, refetch } = useUserSpace();
 
   const { usedPercentage, usedSpace, availableSpace } = useMemo(() => {
     return getBytesToGb(data?.usedSpace, data?.availableSpace);
   }, [data]);
 
+  if (isError) {
+    return (
+      <div>
+        <Button className="cursor-pointer h-8" onClick={() => refetch()}>
+          Refetch
+        </Button>
+      </div>
+    );
+  }
+
   if (isLoading) {
-    return <p>loading</p>;
+    return (
+      <>
+        <Skeleton className="h-8" />
+      </>
+    );
   }
 
   return (
