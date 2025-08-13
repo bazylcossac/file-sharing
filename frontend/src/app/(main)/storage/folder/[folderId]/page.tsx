5396e1260651cef793e6ceafc1164c2a688cb216
bazylcossac <bazyl.cossac@gmail.com>
2025-08-13 20:33:22 +0200

    add delete button to folder page

--- frontend/src/app/(main)/storage/folder/[folderId]/page.tsx
+++ frontend/src/app/(main)/storage/folder/[folderId]/page.tsx
@@ -1,10 +1,23 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useParams } from "next/navigation";
 import React from "react";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 function Folder() {
   const { folderId } = useParams();
-  return <div>folder page {folderId}</div>;
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>folder page {folderId}</div>
+        <div>
+          <Button variant="destructive">
+            <RiDeleteBinLine />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Folder;
